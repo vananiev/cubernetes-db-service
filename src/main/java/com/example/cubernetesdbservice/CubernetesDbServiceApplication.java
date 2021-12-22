5ae67314e35e97d0bb0760ea697afe9680c1a80d
Vitalii Ananev <vananiev@users.noreply.github.com>
2021-12-22 17:29:58 +0300

    init

---
+++ src/main/java/com/example/cubernetesdbservice/CubernetesDbServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cubernetesdbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubernetesDbServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CubernetesDbServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cubernetesdbservice/CubernetesDbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cubernetesdbservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubernetesDbServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
